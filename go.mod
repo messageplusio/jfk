35936c11156a5ef0554387795c48a9f9ac34399f
sarathsp06 <sarath.sp06@gmail.com>
2024-04-26 00:29:48 +0200

    chore(ci): serve HTMX
    Signed-off-by: sarathsp06 <sarath.sp06@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/messageplusio/notifix
 
 go 1.22.2
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	moul.io/http2curl v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,18 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
+
+	"time"
 
 	"github.com/google/uuid"
 )
 
 func main() {
+	// Serve the HTML page
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		id := uuid.New()
-		name := os.Getenv("NAME")
-		fmt.Fprintf(w, "[%s]:Hello, you've requested: %s\n Name:%s", id, r.URL.Path, name)
+		currentTime := time.Now().Format("2006-01-02 15:04:05")
+		fmt.Fprintf(w, `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Current Time</title>
+                <!-- Import HTMX -->
+                <script src="https://unpkg.com/htmx.org"></script>
+            </head>
+            <body>
+                <h1>Current Time Example</h1>
+                <div id="time" hx-get="/time" hx-trigger="every 1s" hx-swap="outerHTML">
+                    %s <br> %s
+                </div>
+            </body>
+            </html>
+        `, currentTime, uuid.New().String())
 	})
 
-	fmt.Println("Server is starting...")
+	// Endpoint to fetch the current time
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now().Format("2006-01-02 15:04:05")
+		fmt.Fprint(w, currentTime)
+	})
+
+	// Start the server
+	fmt.Println("Server starting on http://0.0.0.0:8080/")
 	http.ListenAndServe(":8080", nil)
 }
